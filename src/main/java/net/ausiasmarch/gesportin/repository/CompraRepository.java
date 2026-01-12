31eb97ca156e15a2742ddcfa36d221210458054d
PollySOA <tu-email@example.com>
2026-01-12 22:52:25 +0100

    Actualizar compra..

---
+++ src/main/java/net/ausiasmarch/gesportin/repository/CompraRepository.java
@@ -0,0 +1,9 @@
+package net.ausiasmarch.gesportin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.ausiasmarch.gesportin.entity.CompraEntity;
+
+public interface CompraRepository extends JpaRepository<CompraEntity, Long> {
+
+}
